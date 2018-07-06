a49d2e3de56b8945192ce162f272d36c5590c7c4
sandeeppatel344 <sandeeppatel344@gmail.com>
2018-07-06 13:15:30 +0530

    add validation file

--- src/component/login/layout/login-layout.js
+++ src/component/login/layout/login-layout.js
@@ -1,11 +1,15 @@
 import React from "react";
-import {View} from "react-native";
+import {View,Button} from "react-native";
 import {Field,reduxForm} from "redux-form"
 import {CustomTextbox} from "../../../global/commoncomponent/customTextField"
 const LoginLayout = ()=>{
     return (
         <View>
-            
+            <Field type="numeric" label="Mobile No" name="mobileno" component={CustomTextbox}/>
+            <Field type="default" label="Password" name="password" passwordField={true} component={CustomTextbox}/>
+            <Button title="Login" onPress={}/>
         </View>
     )
 }
+
+export default LoginLayout
--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {createStackNavigator} from "react-navigation"
+import {LoginLayout} from "../component/login/layout/login-layout"
 
 
 const RouterConfig = createStackNavigator({
-  Login: { screen: HomeScreen }
+  Login: { screen: LoginLayout }
 });
